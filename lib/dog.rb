769d3fcdb446767e89006b8da3f013490b06b8bc
devWRM <mcwen@comcast.net>
2020-06-20 17:50:43 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,40 @@
 # Add your code here
+require "pry"
+
+class Dog
+    attr_accessor :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+    def self.print_all
+binding.pry
+
+        @@all.map do |dog|
+            dog.name
+        end
+    end
+
+end
+
+
+
+
+
+
