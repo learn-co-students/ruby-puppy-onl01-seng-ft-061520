3c871f8cd3df08e1699123456af237e71552ea58
Amana Ammishaddai <amana.ammi@gmail.com>
2020-06-26 05:00:28 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,33 @@
 # Add your code here
+
+class Dog 
+
+  attr_reader :name
+
+  @@all = [] 
+
+  def initialize(name)
+    @name = name
+    save
+  end 
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.clear_all
+    @@all.clear
+  end 
+
+  def self.print_all
+ #iterates over each dog in all and puts out to terminal
+    @@all.each do |name|
+    puts self.name
+    end 
+  end
+
+end
